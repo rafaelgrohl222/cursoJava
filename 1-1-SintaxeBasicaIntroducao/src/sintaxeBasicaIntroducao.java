12a66dea80e4089bd397da083cecdee1db58827a
RAFAEL DE FREITAS BRIGIDO <45571427+rafaelgrohl222@users.noreply.github.com>
2019-11-17 11:35:40 -0400

    Curso java básico ao avançado + Orientado a objetos + projetos

---
+++ 1-1-SintaxeBasicaIntroducao/src/sintaxeBasicaIntroducao.java
@@ -0,0 +1,13 @@
+
+public class sintaxeBasicaIntroducao {
+
+	public static void main(String[] args) {
+		
+		System.out.println("package primeiroPragrama;");
+		System.out.println("public class PrimeiroPrograma { }");
+		System.out.println("public static void main(String[] args) {");
+		System.out.println("");
+
+	}
+
+}
